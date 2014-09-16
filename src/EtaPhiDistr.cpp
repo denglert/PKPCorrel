8ae9cb693b65a6cd47d8f4baba2d6c50c0b5eb9b
David <englert.david@gmail.com>
2014-09-16 14:22:44 +0200

    Added Eta Phi distribution plot code

---
+++ src/EtaPhiDistr.cpp
@@ -0,0 +1,200 @@
+#include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <TFile.h>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TMath.h>
+#include <TNtuple.h>
+#include <iostream>
+#include <deque>
+#include <TLorentzVector.h>
+#include "AnalysisFW.h"
+#include "AnalysisBinning.h"
+#include "PIDUtils.h"
+#include "CorrelationUtils.h"
+#include "SetupCustomTrackTree.h"
+
+int main( int argc, const char *argv[] )
+{ 
+
+  if(argc != 4)
+  {
+    std::cerr << "Usage: process <.root file to be preprocessed> <tag> <nEvents>" << std::endl;
+	 exit(1);
+  }
+
+
+ TString inpFilename   = argv[1];
+ std::string tag		  = argv[2];
+ int nEvMax 	  		  = atoi( argv[3] );
+ //TString inpFilename = "../";
+
+ // Binning
+ int nCorrTyp			  = nCorrTyp_; 
+ int *nPtBins = new int[nCorrTyp_];
+
+ for(int TypBin = 0; TypBin < nCorrTyp; TypBin++)
+ { nPtBins[TypBin] = nPtBins_[TypBin]; }
+
+ int nMultiplicityBins_Ana = nMultiplicityBins_Ana_HDR;
+ int nMultiplicityBins_EvM = nMultiplicityBins_EvM_HDR;
+ int nZvtxBins 		      = nZvtxBins_; 
+
+ //////////////////////////////////////
+ //                                  //
+ // ****** Opening input file ****** //
+ //                                  //
+ //////////////////////////////////////
+
+ // Open file
+ TFile *f = TFile::Open(inpFilename);
+ // TFile *f = new TFile(inpFilename, "READ");
+ if ( f->IsZombie() ) {std::cerr << "Error opening file: " << inpFilename << std::endl; exit(-1);}
+ else {std::cout << "File successfully opened." << std::endl;}
+
+ // trackTree
+ TTree *trackTree = (TTree*)f->Get("pptracks/trackTree");
+ Tracks tTracks;
+ bool doCheck = true;
+ setupTrackTree(trackTree, tTracks, doCheck);
+
+ // hiEvtAnalyzer
+ TTree *EvtAna= (TTree*)f->Get("hiEvtAnalyzer/HiTree");
+ int hiNtracks; EvtAna->SetBranchAddress("hiNtracks", &hiNtracks);
+ float vz; EvtAna->SetBranchAddress("vz", &vz);
+
+ // skimanalysis for event selection
+ TTree *SkimAna= (TTree*)f->Get("skimanalysis/HltTree");
+ int pPAcollisionEventSelection; SkimAna->SetBranchAddress("pPAcollisionEventSelection", &pPAcollisionEventSelection);
+ int pileUpBit;                  SkimAna->SetBranchAddress("pVertexFilterCutGplus", &pileUpBit);
+
+ ////////////////////////////////
+ //                            //
+ // ***** Setting output ***** //
+ //                            //
+ ////////////////////////////////
+ 
+ TFile *output = new TFile(Form("./dEtadPhi_distr_%s.root", tag.c_str() ),"RECREATE");
+ output->cd();
+
+ //////////////////////////////
+ //                          //
+ // ***** Initializing ***** //
+ //                          //
+ //////////////////////////////
+ 
+ TH1::SetDefaultSumw2( );
+ TH2::SetDefaultSumw2( );
+
+ AnalysisFW *AnaFW = new AnalysisFW();
+ AnaFW->Setup();
+
+ // dEdxvsp map
+ TH2D *dEdxvsp  = new TH2D ("dEdxVsP",";p(GeV/c);dE/dx", npBins, pMin, pMax, ndEdxBins, dEdxMin, dEdxMax);
+
+ int nParticles = 4;
+
+ int nEtaBins  = 100;
+ int nPhiBins  = 100;
+ double EtaMin = -2.4;
+ double EtaMax =  2.4;
+ double PhiMin = -TMath::Pi();
+ double PhiMax =  TMath::Pi();
+
+ TH2D ***EtaPhiDistr;
+
+ EtaPhiDistr = new TH2D **[nParticles];
+ for (int pid = 0; pid < nParticles; pid++)
+ {
+	EtaPhiDistr[pid] = new TH2D*[nPtBins[pid]];
+
+ 	for (int ptBin = 0; ptBin < nPtBins[pid]; ptBin++)
+	{ EtaPhiDistr[pid][ptBin]  = new TH2D (Form("EtaDistribution id = %d, pt [%.2f - %.2f]", pid, pt(pid, ptBin, 0), pt(pid, ptBin, 1)),";dEta;dPhi", nEtaBins, EtaMin, EtaMax, nPhiBins, PhiMin, PhiMax); }
+ }
+
+ std::cout << "Everything initialized." << std::endl;
+
+ ///////////////////////////
+ //                       //
+ // ***** ANALYISIS ***** //
+ //                       //
+ ///////////////////////////
+ 
+ if (nEvMax == -1) {nEvMax = trackTree->GetEntries();}
+ for (int iEvA = 0; iEvA < nEvMax; iEvA++)
+ {
+	
+	// Event counter info
+	if ( (iEvA % 1000) == 0 )
+	{ std::cout << "Event: " << iEvA << std::endl; }
+
+	// Get current event info
+	EvtAna->GetEntry(iEvA);
+	SkimAna->GetEntry(iEvA);
+
+	AnaFW->CountProcessedEvent();
+
+	// Event Selection
+	if ( !EventSelection( pPAcollisionEventSelection, pileUpBit) ) continue;
+	if ( zvtxbin(vz, nZvtxBins) == -1 ) continue;
+	if ( multiplicitybin_Ana(hiNtracks, nMultiplicityBins_Ana) == -1) continue;
+
+	AnaFW->CountPassedEvent();
+
+	AnaFW->FillnTrk(hiNtracks);
+
+	// Load in tracks
+	trackTree->GetEntry(iEvA);
+
+
+	int nTrk = tTracks.nTrk;
+
+	for (int iTrk = 0; iTrk < nTrk; iTrk++)
+	{
+
+  		float p = tTracks.trkPt[iTrk] * cosh(tTracks.trkEta[iTrk]);
+
+		// *** Track selection *** //
+		if ( !TrackSelection(tTracks, iTrk ) ) continue;
+
+		int PID   = GetPID(p, tTracks.dedx[iTrk]);
+
+		int ptBin_ID = ptbin( PID , tTracks.trkPt[iTrk]);
+		int ptBin_CH = ptbin(   0 , tTracks.trkPt[iTrk]);
+
+		bool isOutsideIdentifHadronPtRange = ( ptBin_ID == -1);
+		bool isOutsideChargedHadronPtRange = ( ptBin_CH == -1);
+
+		double Eta = tTracks.trkEta[iTrk];
+		double Phi = tTracks.trkPhi[iTrk];
+
+		// chadron
+		if( !isOutsideChargedHadronPtRange )
+		{ EtaPhiDistr[0][ptBin_CH]->Fill(Eta,Phi);}
+
+		// pid particle
+		if( (PID != 99) && !isOutsideIdentifHadronPtRange )
+		{
+			EtaPhiDistr[PID][ptBin_ID]->Fill(Eta,Phi);
+		}
+
+		// *** Track selection *** //
+		dEdxvsp->Fill( p, tTracks.dedx[iTrk] );
+
+
+	}
+
+ }
+ 
+ //////////////////////
+ //                  //
+ // **** OUTPUT **** //
+ //                  //
+ //////////////////////
+ 
+ output->Write();
+ output->Close();
+
+
+}

735e7a25f6fa26175765edbcb157678b0c8c27c5
David <englert.david@gmail.com>
2015-02-18 14:50:35 +0100

    Added lxbatch scripts; removed cmsenv

---
+++ batch/task_TrackCorrection.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+### Read in input arguments
+environment=$1
+binary=$2
+workdir=$3
+inputfileDATA=$4
+inputfileMC=$5
+sampleType=$6
+jobid=$7
+nEventsDATA=$8
+nEventsMC=$9
+PIDconfig=$10
+
+# workdir is tdir/arg2
+cd $environment
+eval `scramv1 runtime -sh`
+
+# Task
+rm -rf $workdir; mkdir $workdir;
+cd $workdir; $binary $inputfileDATA $inputfileMC $sampleType $jobid $nEventsDATA $nEventsMC $PIDconfig;
